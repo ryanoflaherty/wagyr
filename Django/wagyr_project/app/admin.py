69220edef0451de3d97122369f4580a5aa95bd7f
ryanoflaherty <ryflare@bu.edu>
2016-04-25 06:47:17 -0400

    jquery for toggle

--- Django/wagyr_project/app/admin.py
+++ Django/wagyr_project/app/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from app.models import Team, Player, Game, Venue
-#Wagyr
+from app.models import Team, Player, Game, Venue, Wagyr
 
 # Register your models here.
 admin.site.register(Team)
 admin.site.register(Player)
 admin.site.register(Game)
-#admin.site.register(Wagyr)
+admin.site.register(Wagyr)
 admin.site.register(Venue)
